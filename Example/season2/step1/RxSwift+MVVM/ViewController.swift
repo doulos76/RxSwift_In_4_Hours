869f1cb91708f7b31e9955cf523933acef92a72b
doulos76 <inojyes@gmail.com>
2020-10-22 09:45:19 +0900

    modify step1

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -13,38 +13,46 @@ import UIKit
 let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 
 class ViewController: UIViewController {
-    @IBOutlet var timerLabel: UILabel!
-    @IBOutlet var editView: UITextView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
-            self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
-        }
+  @IBOutlet var timerLabel: UILabel!
+  @IBOutlet var editView: UITextView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+      self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
     }
-
-    private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
-        guard let v = v else { return }
-        UIView.animate(withDuration: 0.3, animations: { [weak v] in
-            v?.isHidden = !s
-        }, completion: { [weak self] _ in
-            self?.view.layoutIfNeeded()
-        })
+  }
+  
+  private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
+    guard let v = v else { return }
+    UIView.animate(withDuration: 0.3, animations: { [weak v] in
+      v?.isHidden = !s
+      }, completion: { [weak self] _ in
+        self?.view.layoutIfNeeded()
+    })
+  }
+  
+  func downloadJson(_ url: String, _ completion: @escaping (String?) -> Void) {
+    DispatchQueue.global().async {
+      let url = URL(string: url)!
+      let data = try! Data(contentsOf: url)
+      let json = String(data: data, encoding: .utf8)
+      DispatchQueue.main.async {
+        completion(json)
+      }
     }
-
-    // MARK: SYNC
-
-    @IBOutlet var activityIndicator: UIActivityIndicatorView!
-
-    @IBAction func onLoad() {
-        editView.text = ""
-        setVisibleWithAnimation(activityIndicator, true)
-
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
-        
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+  }
+  
+  // MARK: SYNC
+  
+  @IBOutlet var activityIndicator: UIActivityIndicatorView!
+  
+  @IBAction func onLoad() {
+    editView.text = ""
+    self.setVisibleWithAnimation(self.activityIndicator, true)
+    downloadJson(MEMBER_LIST_URL) { json in
+      self.editView.text = json
+      self.setVisibleWithAnimation(self.activityIndicator, false)
     }
+  }
 }
